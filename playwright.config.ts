99ac7155b6a8910c4d727ce908cfcb2f6c41d4b3
Kevinarrasyid <kevinarrasyid3@gmail.com>
2026-05-14 10:13:20 +0700

    add config screenshot & browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'on',
     trace: 'on-first-retry',
   },
 
@@ -39,15 +40,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
