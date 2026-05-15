c4bb1ff7ed91c0c56361b7eb5df9feafc4dce589
Kevinarrasyid <kevinarrasyid3@gmail.com>
2026-05-15 10:17:43 +0700

    Update setting expect timeout to 15s

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  expect: {
+    timeout: 15000,
+  },
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
