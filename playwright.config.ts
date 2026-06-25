f6dd4320ac289f3c4f8c883cbc45cdbed83616e1
Kevinarrasyid <kevinarrasyid3@gmail.com>
2026-06-25 08:39:18 +0700

    Fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,8 +34,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['list'],
-    ['./scripts/db-reporter.ts']
+    ['list']
+    // ['./scripts/db-reporter.ts']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
